7eb05ef315e9bdb13a051bbefb81fffd2daecd9b
Anwar Mohammadi <anwarmohammadi1390@gmail.com>
2025-10-20 17:00:54 +0430

    add analytis chart for state of the orders

--- backend/dbconnection.js
+++ backend/dbconnection.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("orders", "root", "newapp", {
+const sequelize = new Sequelize("orders", "root", "open@123", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
